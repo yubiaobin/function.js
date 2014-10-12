5e54f805c268fd47720613096a4fff63dbccd42e
max <yubiaobin1986@gmail.com>
2014-10-12 11:25:59 +0800

    add a function

--- function.js
+++ function.js
@@ -5,3 +5,17 @@
  * @version $Id$
  */
 
+
+
+
+// 向现有的URL的末尾添加字符串参数
+function addURLParam (url, name , value){
+	url += (url.indexOf("?") == -1 ? "?" : "&");
+	url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+	return url;
+}
+
+// example
+var url = "http://www.oc35.com/index.html";
+var result = addURLParam(url, "search" , "hello");
+console.log(result);    //  http://www.oc35.com/index.html?search=hello 
