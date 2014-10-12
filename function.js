5fdabdfef8fc035e25ee844d94dc57ec87f38416
max <yubiaobin1986@gmail.com>
2014-10-12 11:45:43 +0800

    add a new function

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
-/**
- * 
+/* 
  * @authors maxyu (yubiaobin1986@gmail.com)
  * @date    2014-10-12 11:08:54
  * @version $Id$
  */
+
 
 
 
@@ -22,21 +22,18 @@ console.log(result);    //  http://www.oc35.com/index.html?search=hello
 
 
 
-
-
-
-
-
-
  // 范围函数(本地模拟随机概率)
 function randomFrom(start,end){
     var randomSize = end - start + 1;
     return Math.floor(Math.random() * randomSize + start);
 }
-// example
+// example1
 var num = randomFrom(2,7);
 window.onload = function(){console.log(document.write(num))}  //  2,3,4,5,6,7
-
+// example2
+var targetArray = ['red','orange','yellow','green','cyan','blue','violet','white'];
+var color = targetArray[randomFrom(0,targetArray.length-1)]
+window.onload = function(){console.log(document.write(color))}  // red, orange, yellow, green, cyan, blue, violet, white
 
 
 
